d0f15215764100f0c3c7e5dcdd524a31e6990436
ianshinbro <ianshinbro@gmail.com>
2020-06-14 14:18:37 -0400

    Updating layout

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch, HashRouter } from 'react-router-dom';
 import Header from '../../hoc/header/header'
 import About from '../../Pages/About/About';
@@ -18,7 +18,7 @@ const Layout = (props) => {
           <Route path="/Home" component={Home} exact />
           <Route path="/About" component={About} exact />
           <Route path="/Projects" component={Projects} exact />
-          <Route path="/Poetry" component={Poetry} exact />
+          <Route path="/Poetry" component={Poetry} exact /> 
           <Route path="/Contact" component={Contact} exact />
           <Route component={Home} />
         </Switch>
